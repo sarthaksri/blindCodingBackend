986c83179181b4f5e4b6e59f67592dab54578064
sarthaksri <sarthaksri017@gmail.com>
2024-02-11 12:23:33 +0530

    mongo await

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require("dotenv").config()
 
 
-const connectToMongo = ()=>{
-    mongoose.connect(process.env.mongodburl)
+const connectToMongo = async ()=>{
+   await mongoose.connect(process.env.mongodburl)
     .then(() => console.log('Connected Successfully To Database'))
     .catch(error => console.log('Failed to connect', error))
 }
